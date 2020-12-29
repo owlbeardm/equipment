79cfe0e2044111316e39ff1e97b8e3a868d7a0c6
Nadzeya Ivashchanka <nadzeya.paleika@gmail.com>
2020-12-29 18:54:18 +0300

    #24 configure jest and enzyme

---
+++ src/components/app/app.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+
+import App from './app'
+
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+test('renders without error', () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper).toBeTruthy()
+})
